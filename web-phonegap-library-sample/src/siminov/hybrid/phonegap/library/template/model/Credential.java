3b54f1c52d0cd541fbf54c738d30f501a254a709
Simply Innovative <careers@siminov.com>
2014-12-27 18:45:49 +0530

    Version 1.0 Release 	- Revamp of projects

---
+++ web-phonegap-library-sample/src/siminov/hybrid/phonegap/library/template/model/Credential.java
@@ -0,0 +1,63 @@
+/** 
+ * [SIMINOV FRAMEWORK]
+ * Copyright [2015] [Siminov Software Solution LLP|support@siminov.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+package siminov.hybrid.phonegap.library.template.model;
+
+import siminov.orm.database.Database;
+
+
+public class Credential extends Database {
+
+	//Column Names.
+	public static final String ACCOUNT_ID = "ACCOUNT_ID";
+	public static final String TOKEN = "TOKEN";
+	public static final String ACTIVE = "ACTIVE";
+
+
+	//Class Variables.
+	private String accountId = null;
+	private String token = null;
+	private boolean active;
+
+	public String getAccountId() {
+		return this.accountId;
+	}
+
+	public void setAccountId(String accountId) {
+		this.accountId = accountId;
+	}
+
+	public String getToken() {
+		return this.token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public boolean getActive() {
+		return this.active;
+	}
+	
+	public boolean isActive() {
+		return this.active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+}
