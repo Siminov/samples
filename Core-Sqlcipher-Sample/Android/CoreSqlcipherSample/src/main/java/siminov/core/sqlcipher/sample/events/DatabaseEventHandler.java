e309dc16da5ecf8c1650b36fcafa99afb9b33f2c
Siminov Framework <administrator@siminov.com>
2015-12-27 16:26:37 +0530

    Version 2.0 app changes

---
+++ Core-Sqlcipher-Sample/Android/CoreSqlcipherSample/src/main/java/siminov/core/sqlcipher/sample/events/DatabaseEventHandler.java
@@ -0,0 +1,52 @@
+/** 
+ * [SIMINOV FRAMEWORK]
+ * Copyright [2015] [Siminov Software Solution LLP|support@siminov.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+
+package siminov.core.sqlcipher.sample.events;
+
+import siminov.core.events.IDatabaseEvents;
+import siminov.core.model.DatabaseDescriptor;
+import siminov.core.model.EntityDescriptor;
+import siminov.core.model.EntityDescriptor.Index;
+
+public class DatabaseEventHandler implements IDatabaseEvents {
+
+	public void onDatabaseCreated(DatabaseDescriptor databaseDescriptor) {
+		System.out.print("");
+	}
+
+	public void onTableCreated(DatabaseDescriptor databaseDescriptor, EntityDescriptor entityDescriptor) {
+		System.out.print("");
+	}
+
+	public void onIndexCreated(DatabaseDescriptor databaseDescriptor, EntityDescriptor entityDescriptor, Index index) {
+		System.out.print("");
+	}
+
+	public void onDatabaseDropped(DatabaseDescriptor databaseDescriptor) {
+		// TODO Auto-generated method stub
+	}
+
+	public void onTableDropped(DatabaseDescriptor databaseDescriptor, EntityDescriptor entityDescriptor) {
+		// TODO Auto-generated method stub
+	}
+
+	public void onIndexDropped(DatabaseDescriptor databaseDescriptor, EntityDescriptor entityDescriptor, Index index) {
+		// TODO Auto-generated method stub
+	}
+
+}
